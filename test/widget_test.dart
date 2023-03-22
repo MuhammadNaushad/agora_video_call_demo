a5dc71d9b3fdadc9bca864b1aae039aa0cbf6081
Mohammad Naushad <mdnaushad.yugasa@gmail.com>
2023-03-22 17:48:37 +0530

    first commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,10 +5,9 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:agora_video_call_demo/agora_video_call/agora_video_call.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:agora_video_call_demo/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
